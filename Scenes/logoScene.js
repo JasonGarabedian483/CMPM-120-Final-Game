49f34fbb157013ce002a57d16bd92dc1136fb88e
JasonGarabedian483 <jrgarabe@ucsc.edu>
2026-05-24 15:54:28 -0700

    added logoScene file

---
+++ Scenes/logoScene.js
@@ -0,0 +1,18 @@
+class logoScene extends Phaser.Scene {
+    constructor() {
+        super ('logoscene')
+    }
+    
+    preload() {
+        this.load.path = 'Assets/';
+    }
+
+    create() {
+
+    }
+
+    update() {
+        
+    }
+
+}
