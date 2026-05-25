6ccf7a53e06bff074aa648e36892af6c42067818
JasonGarabedian483 <jrgarabe@ucsc.edu>
2026-05-24 17:21:07 -0700

    added tween to menu icon

--- Scenes/mainMenu.js
+++ Scenes/mainMenu.js
@@ -6,13 +6,25 @@ class mainMenu extends Phaser.Scene {
     preload() {
         this.load.path = 'Assets/';
         this.load.image('background', 'main menu.jpg')
+        //this.load.image('menuicon', 'alien rice.png') <- change alien rice to menu icon
     }
 
     create() {
         let background = this.add.image(1920 / 2, 540, 'background');
             background.setScale(2);
         
-        //let menuIcon = this.add.image(1920 / 2, 400, 'menuicon');
+        /*let menuIcon = this.add.image(1920 / 2, 200, 'menuicon').setOrigin(0.5);
+            menuIcon.setScale(1.5);
+            this.tweens.add({
+                targets: menuIcon,
+                scaleX: 1.75,
+                scaleY: 1.75,
+                duration: 2000,
+                ease: 'Power1',
+                yoyo: true,
+                loop: -1
+            }) */
+        
     }
 
     update() {
