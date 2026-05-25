b177752ff38b2f9db32e1b61fb5527f5d4c9e815
JasonGarabedian483 <jrgarabe@ucsc.edu>
2026-05-24 23:31:40 -0700

    added placeholder buttons and tween effects

--- Scenes/mainMenu.js
+++ Scenes/mainMenu.js
@@ -7,6 +7,9 @@ class mainMenu extends Phaser.Scene {
         this.load.path = 'Assets/';
         this.load.image('background', 'main menu.jpg')
         //this.load.image('menuicon', 'alien rice.png') <- change alien rice to menu icon
+        this.load.image('play', 'placeholder play.png')
+        this.load.image('options', 'placeholder options.png')
+        this.load.image('quit', 'placeholder quit.png')
     }
 
     create() {
@@ -24,7 +27,48 @@ class mainMenu extends Phaser.Scene {
                 yoyo: true,
                 loop: -1
             }) */
-        
+
+        let playButton = this.add.image(1920 / 2, 600, 'play').setScale(.5).setOrigin(.5);
+            playButton.setInteractive({useHandCursor: true});
+            playButton.on("pointerover", () => this.getBigger(playButton));
+            playButton.on("pointerdown", () => {
+
+            });
+            playButton.on("pointerout", ()  => this.getSmaller(playButton))
+        let optionsButton = this.add.image(1920 / 2, 750, 'options').setScale(0.5).setOrigin(0.5);
+            optionsButton.setInteractive({useHandCursor: true});
+            optionsButton.on("pointerover", () => this.getBigger(optionsButton));
+            optionsButton.on("pointerdown", () => {
+                
+            });
+            optionsButton.on("pointerout", () => this.getSmaller(optionsButton));
+        let quitButton = this.add.image(1920 / 2, 900, 'quit').setScale(0.5).setOrigin(0.5);
+            quitButton.setInteractive({useHandCursor: true});
+            quitButton.on("pointerover", () => this.getBigger(quitButton));
+            quitButton.on("pointerdown", () => {
+
+            })
+            quitButton.on("pointerout", () => this.getSmaller(quitButton));
+
+    }
+
+    getBigger(target) {
+        this.tweens.add({
+            targets: target,
+            scaleX: target.scaleX + 0.1,
+            scaleY: target.scaleY + 0.1,
+            duration: 100,
+            ease: 'Power1'
+        });
+    }
+    getSmaller(target) {
+        this.tweens.add({
+            targets: target,
+            scaleX: target.scaleX - 0.1,
+            scaleY: target.scaleY - 0.1,
+            duration: 100,
+            ease: 'Power1'
+        });
     }
 
     update() {
