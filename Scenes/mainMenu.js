2843a390c92bd155fcc6d8363788a254a0b2719b
JasonGarabedian483 <jrgarabe@ucsc.edu>
2026-05-24 23:59:50 -0700

    converted main menu icon from jpg to png to try and fix it not being detected

--- Scenes/mainMenu.js
+++ Scenes/mainMenu.js
@@ -5,7 +5,7 @@ class mainMenu extends Phaser.Scene {
 
     preload() {
         this.load.path = 'assets/';
-        this.load.image('background', 'main menu.jpg')
+        this.load.image('background', 'main_menu.png')
         this.load.image('menuicon', 'alien rice.png')  //<- change alien rice to menu icon
         this.load.image('play', 'placeholder play.png') // <- change placeholder play to actual play button
         this.load.image('options', 'placeholder options.png') // <- change playholder options to actual options button
