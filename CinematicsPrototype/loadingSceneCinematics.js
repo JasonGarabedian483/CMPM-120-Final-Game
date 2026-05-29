290545e8f3b0064246529ec3c0a8ddebc1caa084
Alicia Zhang <alluzhan@ucsc.edu>
2026-05-29 15:19:50 -0700

    Added seperate cinematics prototype files

---
+++ CinematicsPrototype/loadingSceneCinematics.js
@@ -0,0 +1,62 @@
+class loadingSceneCinematics extends Phaser.Scene{
+    constructor(){
+        super('loadingscenecinematics');
+    }
+    init(data){
+        this.nextScene = (data && data.next) ? data.next : 'mainmenucinematics';
+    }
+    preload(){
+        this.load.path = '../assets/images/';
+        this.load.image('background', 'main_menu.png')
+    }
+    create(){  
+        let background = this.add.image(1920 / 2, 540, 'background');
+            background.setScale(2).setTint(0x7f7f7f);
+        
+        let screenCenterX = this.cameras.main.width / 2;
+        let screenCenterY = this.cameras.main.height / 2;
+
+        // Rectangle frame: rectangle with no fill (centerX, centerY, width, height) border (width, color, opacity)
+        this.add.rectangle(screenCenterX, screenCenterY + 50, 600, 50).setStrokeStyle(8, 0xffffff).setScale(2).setOrigin(0.5);
+        
+        // Loading Text
+        this.textObject = this.add.text(
+            screenCenterX - 340,     // x
+            screenCenterY - 200,    // y
+            "Loading...", // text
+            { 
+                font: "125px Courier", // size and font family
+                color: "#ffffff", // text color
+            } 
+        );
+
+        // Fade in Scene
+        this.cameras.main.fadeIn(500);
+
+        // Loading Bar
+        this.tweens.chain({ //bar: rectangle(x, y, fade at x of bar, height, color).setOrigin(Left to Right)
+            targets: this.add.rectangle(screenCenterX - 580, screenCenterY + 50, 0, 30, 0xffffff).setScale(2).setOrigin(0, 0.5), 
+            tweens: [
+                {
+                    width: 580, 
+                    alpha: 1, // Opacity: full
+                    duration: 2000, //duration of action
+                    delay: 200, // delay start
+                    ease: 'Power2', //rate of change of animation
+                },
+                {
+                    duration: 2000,
+                    delay: 200, // delay start
+                    ease: 'Sine.easeOut', //rate of change of animation
+                }
+            ],
+            onComplete: () => {
+                this.cameras.main.fadeOut(300);
+                this.cameras.main.once('camerafadeoutcomplete', () => {
+                    this.scene.start(this.nextScene);
+                });
+            },
+        });
+    }
+    update(){}
+}
---
+++ CinematicsPrototype/logoSceneCinematics.js
@@ -0,0 +1,95 @@
+class logoSceneCinematics extends Phaser.Scene {
+    constructor() {
+        super ('logoscenecinematics')
+    }
+    
+    preload() {
+        this.load.path = '../assets/images/';
+        this.load.image('titleLogo', 'titleLogo.png');
+
+    }
+
+    create() {
+        let centerX = this.cameras.main.width / 2;
+        let centerY = this.cameras.main.height / 2;
+
+        this.cameras.main.setBackgroundColor('#000000');
+
+        //starry bacckground
+        for(let i = 0; i < 100; i++) {
+            let star = this.add.circle(
+                Phaser.Math.Between(0, 1920),
+                Phaser.Math.Between(0, 1080),
+                Phaser.Math.Between(1, 3),
+                0xffffff,
+                Phaser.Math.FloatBetween(0.3, 1)
+            );
+
+            this.tweens.add({
+                targets: star,
+                alpha: 0.2,
+                duration: Phaser.Math.Between(500, 1500),
+                yoyo: true,
+                repeat: -1
+            });
+        }
+
+        //logo starts invisible
+        let logo = this.add.image(centerX - 60, centerY, 'titleLogo');
+        logo.setScale(1).setOrigin(0.5);
+        logo.setAlpha(0);
+
+        this.cameras.main.fadeIn(1000);
+
+        //logo animation -> scales up and fades in
+        this.tweens.add({
+            targets: logo,
+            scale: 2,
+            alpha: 1,
+            duration: 4000,
+            ease: 'Back.Out'
+        });
+
+        //pulsing effect for logo
+        this.time.delayedCall(3000, () => {
+            this.tweens.add({
+                targets: logo,
+                scale: 1.8,
+                duration: 1000,
+                yoyo: true,
+                repeat: 2
+            });
+
+            // camera shake
+            this.cameras.main.shake(300, 0.002);
+        });
+
+        //end text
+        let text = this.add.text(
+            centerX,
+            900,
+            "PREPARE FOR LAUNCH...",
+            {
+                font: "48px Pixelify Sans",
+                color: "#ffffff"
+            }
+        ).setOrigin(0.5).setAlpha(0);
+
+        this.time.delayedCall(3500, () => {
+            this.tweens.add({
+                targets: text,
+                alpha: 1,
+                duration: 800
+            });
+        });
+
+       
+        this.time.delayedCall(8000, () => {
+            this.cameras.main.fadeOut(1000);
+            this.cameras.main.once('camerafadeoutcomplete', () => {
+                this.scene.start('loadingscenecinematics'); 
+            });
+        });
+    }
+
+}
