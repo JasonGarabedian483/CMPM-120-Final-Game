e3d6764b80bdf547a0b3bdb9e2f3308d66800f70
JasonGarabedian483 <jrgarabe@ucsc.edu>
2026-06-01 18:34:18 -0700

    fixed game.js

--- game.js
+++ game.js
@@ -22,7 +22,7 @@ const game = new Phaser.Game({
             debug: true
         }
     },
-    //scene: [logoScene, loadingScene, mainMenu, Level1, Level2, Level3, Credits, ReplayScene, Audio, Timer, Summary, TestingUI],
-    scene: [Level1],
+    scene: [logoScene, loadingScene, mainMenu, Level1, Level2, Level3, Credits, ReplayScene, Audio, Timer, Summary, TestingUI],
+    //scene: [Level1],
     title: "Placeholder Title",
 });
